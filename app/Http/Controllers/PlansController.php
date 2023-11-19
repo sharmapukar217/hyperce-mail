7e91eaa4b0f695fd077fd4f412c34ff1699eb288
Pukar Sharma <sharmapukar217@gmail.com>
2023-11-19 17:53:19 +0000

    added plans

--- app/Http/Controllers/PlansController.php
+++ app/Http/Controllers/PlansController.php
@@ -8,6 +8,7 @@ use App\Models\PlanType;
 use App\Facades\HyperceMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
 class PlansController extends Controller
@@ -35,22 +36,26 @@ class PlansController extends Controller
     }
 
 
-    public function update(Request $request): View {
+    public function update(Request $request) {
         $currentDate = Carbon::now();
         $selectedPlanId = $request->get('plan_id');
         $workspaceId = HyperceMail::currentWorkspaceId();
 
         // TODO: ADD EXPIRY DATE FROM FRONTEND
-        Plan::updateOrInsert(["workspace_id"=>$workspaceId],[
+        $result = Plan::updateOrInsert(["workspace_id"=>$workspaceId],[
             "plan_id" => $selectedPlanId,
             "enrolled_at" => $currentDate,
             "expires_at" => $currentDate->addDays(14),
         ]);
 
-        return view('plans.index', [
-            "currentPlan" => $this->getCurrentPlan(),
-            "availablePlans" => $this->getAvailablePlans()
-        ]);
+        if($result) {
+            return view('plans.index', [
+                "currentPlan" => $this->getCurrentPlan(),
+                "availablePlans" => $this->getAvailablePlans()
+            ]);
+        } else {
+            return redirect(route("dashboard"));
+        }
     }
 
 }
