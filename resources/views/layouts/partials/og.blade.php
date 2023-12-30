4e2a87c202d720577f805c63e2317114144334cc
Pukar Sharma <sharmapukar217@gmail.com>
2023-12-30 23:42:32 +0545

    added og tags

---
+++ resources/views/layouts/partials/og.blade.php
@@ -0,0 +1,10 @@
+<meta property="og:title" content="{{ config('app.name') }}">
+<meta property="og:description" content="An opinionated email marketing platform powered by Hyperce.">
+<meta property="og:image" content="/img/og_preview.webp">
+<meta property="og:url" content="https://zippybox.hyperce.io/">
+<meta property="og:type" content="website">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="{{ config('app.name') }}">
+<meta name="twitter:description" content="An opinionated email marketing platform powered by Hyperce.">
+<meta name="twitter:image" content="/img/og_preview.webp">
